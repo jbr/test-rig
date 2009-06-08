b39a19961bb3db6f3af1c6f1db1d6c9e9fa28146
Jacob Rothstein <github@jacobrothstein.com>
2009-06-07 22:11:58 -0700

    update to latest (extraction)

--- lib/neato_assertions.rb
+++ lib/neato_assertions.rb
@@ -3,12 +3,19 @@ module NeatoAssertions
   @@context_lines = 2
   
   def method_missing_with_neato_assertions(method, *args)
-    if method.to_s =~ /^assert_(not_)?([a-z_]+)\??/ && args.first.respond_to?(:"#{$2}?")
-      actual = args.first.send(:"#{$2}?")
-      assert $1 ? !actual : actual
-    else
-      method_missing_without_neato_assertions(method, *args)
+    if method.to_s =~ /^assert_(not_)?([a-z_]+)/
+      method_name = $2.to_sym
+      if args.length == 1 && args.first.respond_to?(:"#{method_name}?")
+        actual = args.first.send :"#{method_name}?"
+        assert $1 ? !actual : actual
+        return
+      elsif args.length == 2 && args.last.respond_to?(method_name)
+        send :"assert#{$1}_equal", args.first, args.last.send(method_name)
+        return
+      end
     end
+    
+    method_missing_without_neato_assertions(method, *args)
   end
 
   def self.included(klass)
